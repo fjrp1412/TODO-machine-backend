85aed1c48a6b49d48f34d08321e022d9ca312145
Francisco Ruiz <fruz.rc@gmail.com>
2022-07-27 16:20:04 -0400

    format serializer in workspace

--- workspace/serializers.py
+++ workspace/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import Workspace
 
+
 class WorkspaceSerializer(ModelSerializer):
     class Meta:
         model = Workspace
